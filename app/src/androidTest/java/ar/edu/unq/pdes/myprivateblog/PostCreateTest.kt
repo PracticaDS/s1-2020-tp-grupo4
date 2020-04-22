61184042c284a4b675fcf740f0339966b3093897
Rodrigo Papadopulo <rpapadopulo.2106@gmail.com>
2020-04-22 00:32:15 -0300

    Adding integration test for PostCreating

--- app/src/androidTest/java/ar/edu/unq/pdes/myprivateblog/PostCreateTest.kt
+++ app/src/androidTest/java/ar/edu/unq/pdes/myprivateblog/PostCreateTest.kt
@@ -1,18 +1,32 @@
 package ar.edu.unq.pdes.myprivateblog
 
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
+import org.junit.Assert
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 @LargeTest
-class PostsCreateTest {
+class PostsCreateTest : BaseInjectedTest(){
+
+    @get:Rule
+    var activityRule: ActivityScenarioRule<MainActivity> =
+        ActivityScenarioRule(MainActivity::class.java)
 
     @Test
     fun whenTappingOnNewPostFab_postCreationScreenShouldOpen() {
         R.id.create_new_post.clickButton()
         R.id.title.isMatchingWithHint(R.string.hint_post_title)
+    }
+    
+    @Test
+    fun whenEnteringPostCreate_shouldShowActionButtons(){
+        R.id.create_new_post.clickButton()
+        R.id.btn_save.isDisplayedInView()
+        R.id.btn_close.isDisplayedInView()
     }
 
     @Test
@@ -24,9 +38,40 @@ class PostsCreateTest {
         R.id.title.fillText(postTitle)
         R.id.body.fillText(bodyText)
 
-        R.id.btn_save.isDisplayedInView()
+        R.id.title.isMatchingWithValue(postTitle)
+        R.id.body.isMatchingWithValue(bodyText)
+    }
+
+    @Test
+    fun whenClickingBtnClose_shouldGoBack(){
+        R.id.create_new_post.clickButton()
+        R.id.btn_close.clickButton()
+
+        R.id.create_new_post.isDisplayedInView()
+    }
+
+    @Test
+    fun whenClickingSave_shouldSaveAndShowDetail(){
+        var initialSize = blogEntriesService.getDataCount()
+        val postTitle = "Test title post"
+        val bodyText = "Test body text"
+
+        R.id.create_new_post.clickButton()
+        R.id.title.fillText(postTitle)
+        R.id.body.fillText(bodyText)
+
         R.id.btn_save.clickButton()
 
+        // Verify that saves information
+        val finalSize = blogEntriesService.getDataCount()
+        Assert.assertSame(initialSize + 1, finalSize)
+
+        // Verify that shows button of post detail fragment
+        R.id.btn_edit.isDisplayedInView()
+        R.id.btn_back.isDisplayedInView()
+        R.id.btn_delete.isDisplayedInView()
+
+        // Verify that shows the correct information
         R.id.title.isMatchingWithValue(postTitle)
     }
 
