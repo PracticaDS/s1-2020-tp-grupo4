7d3b9b682304f8963e03ed22828b828ce5d1539f
Juanma Vallejos <juanmanuel.vallejos@fravega.com.ar>
2020-04-25 23:27:14 -0300

    Se agrega test con visualizacion y click de popup al eliminar post

--- app/src/androidTest/java/ar/edu/unq/pdes/myprivateblog/PostDeleteTest.kt
+++ app/src/androidTest/java/ar/edu/unq/pdes/myprivateblog/PostDeleteTest.kt
@@ -1,5 +1,12 @@
 package ar.edu.unq.pdes.myprivateblog
 
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.RootMatchers.isDialog
+import androidx.test.espresso.matcher.RootMatchers.isPlatformPopup
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.isFocusable
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
@@ -30,6 +37,8 @@ class PostsDeleteTest : BaseInjectedTest() {
         var initialSize = blogEntriesService.getDataCount()
         R.id.btn_delete.isDisplayedInView()
         R.id.btn_delete.clickButton()
+        R.id.btn_delete_confirm.isDisplayedInPopup()
+        R.id.btn_delete_confirm.clickPopupButton()
         val finalSize = blogEntriesService.getDataCount()
         Assert.assertSame(initialSize - 1, finalSize)
     }
