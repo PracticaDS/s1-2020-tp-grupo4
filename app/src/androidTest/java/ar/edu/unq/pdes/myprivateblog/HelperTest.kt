4e80d28bbe7fbb9b4e024a939dda278a2c557dab
Sergio Ivanic <ivanicsergio@gmail.com>
2020-04-26 21:04:45 -0300

    Added Espresso Web dependency. Added assertions for WebView element in Post Creation and Edit e2e test.

--- app/src/androidTest/java/ar/edu/unq/pdes/myprivateblog/HelperTest.kt
+++ app/src/androidTest/java/ar/edu/unq/pdes/myprivateblog/HelperTest.kt
@@ -5,6 +5,13 @@ import androidx.test.espresso.ViewInteraction
 import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.web.assertion.WebViewAssertions
+import androidx.test.espresso.web.model.Atoms
+import androidx.test.espresso.web.sugar.Web
+import androidx.test.espresso.web.webdriver.DriverAtoms
+import androidx.test.espresso.web.webdriver.Locator
+import org.hamcrest.CoreMatchers.containsString
 
 fun Int.isMatchingWithValue(anStringValue : String): ViewInteraction = Espresso.onView(ViewMatchers.withId(this)).check(ViewAssertions.matches(ViewMatchers.withText(anStringValue)))
 
@@ -17,3 +24,13 @@ fun Int.fillText(text : String) : ViewInteraction = Espresso.onView(ViewMatchers
 fun Int.clearText() : ViewInteraction = Espresso.onView(ViewMatchers.withId(this)).perform(ViewActions.clearText())
 
 fun Int.isMatchingWithHint(hintResource : Int): ViewInteraction = Espresso.onView(ViewMatchers.withId(this)).check(ViewAssertions.matches(ViewMatchers.withHint(hintResource)))
+
+fun Int.webViewIsMatchingWithValue(anStringValue : String): Web.WebInteraction<String> =
+    Web.onWebView(withId(this))
+        .withElement(DriverAtoms.findElement(Locator.TAG_NAME, "html"))
+        .check(
+            WebViewAssertions.webMatches(
+                Atoms.getCurrentUrl(),
+                containsString(anStringValue)
+            )
+        )
