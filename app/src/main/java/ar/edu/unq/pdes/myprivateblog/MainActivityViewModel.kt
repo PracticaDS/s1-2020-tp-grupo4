635f48128484f23ea8277047c242207a20fb25f4
Sergio Ivanic <ivanicsergio@gmail.com>
2020-07-03 21:32:09 -0300

    Changed logic for sync  upload after logging in and download sync when pressing the button in the drawer menu.

--- app/src/main/java/ar/edu/unq/pdes/myprivateblog/MainActivityViewModel.kt
+++ app/src/main/java/ar/edu/unq/pdes/myprivateblog/MainActivityViewModel.kt
@@ -1,10 +1,16 @@
 package ar.edu.unq.pdes.myprivateblog
 
 import androidx.lifecycle.ViewModel
+import ar.edu.unq.pdes.myprivateblog.services.BlogEntriesSyncingService
+import ar.edu.unq.pdes.myprivateblog.services.EncryptionService
+import ar.edu.unq.pdes.myprivateblog.services.drive.GoogleDriveService
 import javax.inject.Inject
 
 class MainActivityViewModel  @Inject constructor(
     /* add injectable dependencies here */
+    val encryptionService: EncryptionService,
+    val blogEntriesSyncingService: BlogEntriesSyncingService,
+    val googleDriveService: GoogleDriveService
 ) : ViewModel() {
 
 }
--- app/src/main/java/ar/edu/unq/pdes/myprivateblog/screens/sign/OauthSignViewModel.kt
+++ app/src/main/java/ar/edu/unq/pdes/myprivateblog/screens/sign/OauthSignViewModel.kt
@@ -1,15 +1,21 @@
 package ar.edu.unq.pdes.myprivateblog.screens.sign
 
 import android.content.Context
+import androidx.fragment.app.Fragment
 import ar.edu.unq.pdes.myprivateblog.BaseViewModel
+import ar.edu.unq.pdes.myprivateblog.R
 import ar.edu.unq.pdes.myprivateblog.services.BlogEntriesService
+import ar.edu.unq.pdes.myprivateblog.services.BlogEntriesSyncingService
 import ar.edu.unq.pdes.myprivateblog.services.EncryptionService
 import ar.edu.unq.pdes.myprivateblog.services.drive.GoogleDriveService
+import com.google.android.material.snackbar.Snackbar
 import javax.inject.Inject
 
 class OauthSignViewModel @Inject constructor(
     blogEntriesService: BlogEntriesService,
     private val googleDriveService: GoogleDriveService,
+    private val blogEntriesSyncingService: BlogEntriesSyncingService,
+    private val encryptionService: EncryptionService,
     context: Context
 ): BaseViewModel(blogEntriesService, context) {
 
@@ -19,4 +25,17 @@ class OauthSignViewModel @Inject constructor(
         }
     }
 
+    fun uploadPosts(fragment: Fragment) {
+        if (SYNCING_FEATURE_ENABLED) {
+            val secretKey = encryptionService.retrieveSecretKey()
+            if (secretKey != null) {
+                blogEntriesSyncingService.uploadUnsyncedBlogEntries(secretKey)
+            } else {
+                Snackbar.make(
+                    fragment.view!!,
+                    R.string.could_not_sync_try_again, Snackbar.LENGTH_LONG)
+                    .show();
+            }
+        }
+    }
 }
